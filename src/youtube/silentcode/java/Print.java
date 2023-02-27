eabb42f7430036aee8561d4fe008277f2eb7a59c
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-02-27 22:58:10 +0530

    Print Hello World

---
+++ src/youtube/silentcode/java/Print.java
@@ -0,0 +1,16 @@
+package youtube.silentcode.java;
+
+public class Print {
+    //lets go now Learning Fist Programing Print Hello World
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //and now we learning what is Println or print statement difference
+        //print statement
+        System.out.print("Good");
+        System.out.print("Morning");
+        // That's See Difference
+        System.out.println("Good");
+        System.out.println("Night");
+        //Good Bye Next Lesson See You Thanks for Watching my video bye
+    }
+}
