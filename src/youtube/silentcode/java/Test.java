3f5260c553fd2cdddde3c7c5792827024d1964dd
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-02-27 13:49:06 +0530

    first lesson end

--- src/youtube/silentcode/java/Test.java
+++ src/youtube/silentcode/java/Test.java
@@ -1,5 +1,13 @@
 package youtube.silentcode.java;
 
+// create class
 public class Test {
-
+    // now we leaning Shortcut key
+    // create main method ShortCut key
+    public static void main(String[] args) {
+        //then now learning print command shortcut key
+        System.out.println("Hello");
+        //lets go run program checking
+    }
 }
+// ok thanks you watching my video see you next lesson bye
